13efa7ee2bf6b7d6979f1aac3501db05ba014ed0
Graham Dennis <graham.dennis@gmail.com>
2024-07-03 16:28:10 +1000

    simple in-memory store

---
+++ attribute-store/src/inmemory.rs
@@ -0,0 +1,84 @@
+use crate::store::{
+    AttributeStore, AttributeStoreError, AttributeValue, BootstrapSymbol, Entity, EntityId,
+    EntityLocator, Symbol, ValueType,
+};
+use async_trait::async_trait;
+use std::collections::HashMap;
+use std::sync::Mutex;
+
+#[derive(Debug)]
+struct InMemoryAttributeStore {
+    entities: Mutex<HashMap<EntityId, Entity>>,
+}
+
+impl InMemoryAttributeStore {
+    pub fn new() -> Self {
+        let mut entities: HashMap<EntityId, Entity> = HashMap::new();
+        for entity in Self::bootstrap_entities().into_iter() {
+            entities.insert(entity.entity_id, entity);
+        }
+        InMemoryAttributeStore {
+            entities: Mutex::new(entities),
+        }
+    }
+
+    fn bootstrap_entities() -> Vec<Entity> {
+        vec![
+            BootstrapSymbol::EntityId.into(),
+            BootstrapSymbol::SymbolName.into(),
+            BootstrapSymbol::ValueType.into(),
+            BootstrapSymbol::ValueTypeEnum(ValueType::Text).into(),
+            BootstrapSymbol::ValueTypeEnum(ValueType::EntityReference).into(),
+            BootstrapSymbol::ValueTypeEnum(ValueType::Bytes).into(),
+        ]
+    }
+}
+
+#[async_trait]
+impl AttributeStore for InMemoryAttributeStore {
+    async fn get_entity(
+        &self,
+        entity_locator: &EntityLocator,
+    ) -> Result<Entity, AttributeStoreError> {
+        use AttributeStoreError::*;
+
+        let locked_entities = self
+            .entities
+            .lock()
+            .map_err(|_| InternalError("task failed while holding lock"))?;
+        let entity =
+            match entity_locator {
+                EntityLocator::EntityId(entity_id) => locked_entities.get(entity_id),
+                EntityLocator::Symbol(symbol) => {
+                    let symbol_name_symbol: Symbol = BootstrapSymbol::SymbolName.into();
+                    let expected_attribute_value = AttributeValue::String(symbol.clone().into());
+                    locked_entities
+                        .iter()
+                        .map(|(_, entity)| entity)
+                        .find(|entity| {
+                            entity.attributes.get(&symbol_name_symbol).is_some_and(
+                                |attribute_value| attribute_value.eq(&expected_attribute_value),
+                            )
+                        })
+                }
+            }
+            .ok_or_else(|| EntityNotFound(entity_locator.clone()))?;
+
+        Ok(entity.clone())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn it_works() {
+        let store = InMemoryAttributeStore::new();
+        let entity_id_entity = store
+            .get_entity(&EntityLocator::EntityId(BootstrapSymbol::EntityId.into()))
+            .await
+            .unwrap();
+        assert_eq!(entity_id_entity, BootstrapSymbol::EntityId.into());
+    }
+}
---
+++ attribute-store/src/store.rs
@@ -0,0 +1,167 @@
+use async_trait::async_trait;
+use regex::Regex;
+use std::collections::HashMap;
+use std::sync::OnceLock;
+use thiserror::Error;
+
+static SYMBOL_REGEX_CELL: OnceLock<Regex> = OnceLock::new();
+
+fn symbol_regex<'a>() -> &'a Regex {
+    SYMBOL_REGEX_CELL.get_or_init(|| {
+        Regex::new(r"[[:alnum:]_$.,/<>:;@#%^ ]{1,60}").expect("Failed to compile symbol regex")
+    })
+}
+
+#[derive(Error, Debug)]
+pub enum AttributeStoreError {
+    #[error("name `{0}` is not a valid symbol name")]
+    InvalidSymbolName(String),
+    #[error("internal error: `{0}`")]
+    InternalError(&'static str),
+    #[error("entity not found (locator: `{0:?}`)")]
+    EntityNotFound(EntityLocator),
+}
+
+#[derive(Eq, PartialEq, Hash, Debug, Copy, Clone)]
+pub struct EntityId(i64);
+
+#[derive(Eq, PartialEq, Hash, Debug, Clone)]
+pub struct Symbol(String);
+
+impl TryFrom<String> for Symbol {
+    type Error = AttributeStoreError;
+
+    fn try_from(string: String) -> Result<Self, Self::Error> {
+        if !symbol_regex().is_match(&string) {
+            Err(AttributeStoreError::InvalidSymbolName(string))
+        } else {
+            Ok(Symbol(string))
+        }
+    }
+}
+
+impl From<Symbol> for String {
+    fn from(value: Symbol) -> Self {
+        let Symbol(inner) = value;
+        inner
+    }
+}
+
+#[derive(Eq, PartialEq, Debug, Clone)]
+pub enum EntityLocator {
+    EntityId(EntityId),
+    Symbol(Symbol),
+}
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub struct Entity {
+    pub entity_id: EntityId,
+    // Should the key here be InternalEntityId?
+    pub attributes: HashMap<Symbol, AttributeValue>,
+}
+
+#[derive(Eq, PartialEq, Hash, Debug, Clone)]
+pub enum AttributeValue {
+    String(String),
+    EntityId(EntityId),
+    Bytes(Vec<u8>),
+}
+
+#[async_trait]
+pub trait AttributeStore: Send + Sync + 'static {
+    async fn get_entity(
+        &self,
+        entity_locator: &EntityLocator,
+    ) -> Result<Entity, AttributeStoreError>;
+}
+
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+pub enum BootstrapSymbol {
+    EntityId,
+    SymbolName,
+    ValueType,
+    ValueTypeEnum(ValueType),
+}
+
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+pub enum ValueType {
+    Text,
+    EntityReference,
+    Bytes,
+}
+
+impl From<BootstrapSymbol> for EntityId {
+    #[inline]
+    fn from(value: BootstrapSymbol) -> Self {
+        match value {
+            BootstrapSymbol::EntityId => EntityId(0),
+            BootstrapSymbol::SymbolName => EntityId(1),
+            BootstrapSymbol::ValueType => EntityId(2),
+            BootstrapSymbol::ValueTypeEnum(value_type) => EntityId::from(value_type),
+        }
+    }
+}
+
+impl From<ValueType> for EntityId {
+    #[inline]
+    fn from(value: ValueType) -> Self {
+        match value {
+            ValueType::Text => EntityId(3),
+            ValueType::EntityReference => EntityId(4),
+            ValueType::Bytes => EntityId(5),
+        }
+    }
+}
+
+impl From<BootstrapSymbol> for Symbol {
+    fn from(value: BootstrapSymbol) -> Self {
+        match value {
+            BootstrapSymbol::EntityId => Symbol("@id".to_string()),
+            BootstrapSymbol::SymbolName => Symbol("@symbolName".to_string()),
+            BootstrapSymbol::ValueType => Symbol("@valueType".to_string()),
+            BootstrapSymbol::ValueTypeEnum(value_type) => Symbol::from(value_type),
+        }
+    }
+}
+
+impl From<ValueType> for Symbol {
+    fn from(value: ValueType) -> Self {
+        match value {
+            ValueType::Text => Symbol("@valueType/text".to_string()),
+            ValueType::EntityReference => Symbol("@valueType/entityRef".to_string()),
+            ValueType::Bytes => Symbol("@valueType/bytes".to_string()),
+        }
+    }
+}
+
+impl BootstrapSymbol {
+    fn value_type(self: BootstrapSymbol) -> Option<EntityId> {
+        match self {
+            BootstrapSymbol::EntityId => Some(ValueType::EntityReference.into()),
+            BootstrapSymbol::SymbolName => Some(ValueType::Text.into()),
+            BootstrapSymbol::ValueType => Some(ValueType::EntityReference.into()),
+            BootstrapSymbol::ValueTypeEnum(_) => None,
+        }
+    }
+}
+
+impl From<BootstrapSymbol> for Entity {
+    fn from(value: BootstrapSymbol) -> Self {
+        let symbol: Symbol = value.into();
+        let mut attributes = HashMap::from([(
+            BootstrapSymbol::SymbolName.into(),
+            AttributeValue::String(symbol.into()),
+        )]);
+        if let Some(value_type_entity_id) = value.value_type() {
+            attributes.insert(
+                BootstrapSymbol::ValueType.into(),
+                AttributeValue::EntityId(value_type_entity_id),
+            );
+        }
+
+        Entity {
+            entity_id: value.into(),
+            attributes,
+        }
+    }
+}
