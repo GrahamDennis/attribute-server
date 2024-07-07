4fc54a3728452a5b177615f3cf16ee93aabb41f4
Graham Dennis <graham.dennis@gmail.com>
2024-07-07 11:25:39 +1000

    support shell completions

--- attribute-cli/src/main.rs
+++ attribute-cli/src/main.rs
@@ -1,10 +1,12 @@
+use anyhow::format_err;
 use attribute_grpc_api::grpc::attribute_store_client::AttributeStoreClient;
 use attribute_grpc_api::grpc::{PingRequest, QueryEntitiesRequest};
-use clap::{Parser, Subcommand};
+use clap::{CommandFactory, Parser, Subcommand};
+use clap_complete::Shell;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::PathBuf;
-use tonic::transport::Endpoint;
+use tonic::transport::{Channel, Endpoint};
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::EnvFilter;
 
@@ -22,11 +24,23 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+    /// Send ping request
     Ping,
+    /// Query for entities
     QueryEntities {
         #[clap(short, long, default_value = "content")]
         json: PathBuf,
     },
+    /// Generate shell completions script
+    GenerateCompletions {
+        /// shell to generate completions for
+        #[clap(short, long)]
+        shell: Option<Shell>,
+    },
+}
+
+fn print_completions<G: clap_complete::Generator>(gen: G, cmd: &mut clap::Command) {
+    clap_complete::generate(gen, cmd, cmd.get_name().to_string(), &mut std::io::stdout());
 }
 
 #[tokio::main]
@@ -41,14 +55,11 @@ async fn main() -> anyhow::Result<()> {
 
     let cli = Cli::parse();
 
-    let channel = Endpoint::from_shared(cli.endpoint)?.connect().await?;
-
-    let mut attribute_store_client = AttributeStoreClient::new(channel);
-
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
         Commands::Ping => {
+            let mut attribute_store_client = create_attribute_store_client(&cli.endpoint).await?;
             let response = attribute_store_client.ping(PingRequest {}).await?;
             println!("response: {:?}", response);
 
@@ -59,6 +70,7 @@ async fn main() -> anyhow::Result<()> {
             let reader = BufReader::new(file);
             let query_entities_request: QueryEntitiesRequest = serde_json::from_reader(reader)?;
 
+            let mut attribute_store_client = create_attribute_store_client(&cli.endpoint).await?;
             let response = attribute_store_client
                 .query_entities(query_entities_request)
                 .await?;
@@ -67,5 +79,21 @@ async fn main() -> anyhow::Result<()> {
 
             Ok(())
         }
+        Commands::GenerateCompletions { shell } => Ok(print_completions(
+            shell
+                .or_else(|| Shell::from_env())
+                .ok_or_else(|| format_err!("specify shell with `--shell`"))?,
+            &mut Cli::command(),
+        )),
     }
+}
+
+async fn create_attribute_store_client(
+    endpoint: &str,
+) -> anyhow::Result<AttributeStoreClient<Channel>> {
+    let channel = Endpoint::from_shared(endpoint.to_string())?
+        .connect()
+        .await?;
+
+    Ok(AttributeStoreClient::new(channel))
 }
