4f338088427d70c36d39b5f947ce6dc61d683c23
Graham Dennis <graham.dennis@gmail.com>
2024-07-07 13:09:14 +1000

    Make serde complain when deserialising with unknown fields

--- attribute-grpc-api/build.rs
+++ attribute-grpc-api/build.rs
@@ -2,7 +2,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .type_attribute(
             ".",
-            "#[derive(serde::Serialize, serde::Deserialize)] #[serde(rename_all = \"snake_case\")]",
+            "#[derive(serde::Serialize, serde::Deserialize)]\
+             #[serde(rename_all = \"snake_case\", deny_unknown_fields)]",
         )
         .compile(&["proto/attribute.proto"], &["proto/"])?;
     Ok(())
